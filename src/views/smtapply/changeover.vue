f610eb964a77850321b5f886656b49b4a3d9e846
“liangfengguang” <1447808929@qq.com>
2025-06-12 14:41:30 +0800

    更新

--- src/views/smtapply/changeover.vue
+++ src/views/smtapply/changeover.vue
@@ -55,11 +55,12 @@
                 </el-select>
               </el-form-item>
               <el-form-item label="产品名">{{ form.product }}</el-form-item>
-              <el-form-item label="软件名">{{ form.program }}</el-form-item>
-              <!-- <el-form-item label="软件版本">{{
+              <!-- <el-form-item label="软件名">{{ form.program }}</el-form-item> -->
+              <el-form-item label="软件名">-----</el-form-item>
+              <el-form-item label="软件版本">{{
                 form.productVer
-              }}</el-form-item> -->
-              <el-form-item label="软件版本">-----</el-form-item>
+              }}</el-form-item>
+              <!-- <el-form-item label="软件版本">-----</el-form-item> -->
               <el-form-item label="BOM版本">{{ form.bomVer }}</el-form-item>
             </el-form>
             <div v-show="form.order !== ''" class="qrcode">
