fa04f7ceb0e3ca7801e736ea2737f73e3fd40b06
“liangfengguang” <1447808929@qq.com>
2025-07-08 15:55:32 +0800

    更新

--- src/api/assemble.js
+++ src/api/assemble.js
@@ -3,8 +3,34 @@ import request from "@/request/assembleRequest";
 export function processSumbit(data) {
 
     return request({
-        url: "Device2Mes/AssemblyRework",
+        url: "/AssSpec/AssemblyRework",
         method: "post",
         data,
     });
 }
+//智驾线过程码--创建过程码
+export function ExecuteAssGenerateSN(data) {
+
+    return request({
+        url: "/AssSpec/ExecuteAssGenerateSN",
+        method: "post",
+        data,
+    });
+}
+//智驾线过程码--查询过程码
+export function GetAssGenerateRecord(data) {
+
+    return request({
+        url: "/AssSpec/GetAssGenerateRecord",
+        method: "post",
+        data,
+    });
+}
+//智驾线过程码--获取明细
+export function GetAssGenerateSN(data) {
+
+    return request({
+        url: "/AssSpec/GetAssGenerateSN",
+        method: "post",
+        data,
+    }); }
--- src/views/assemble/processOrder.vue
+++ src/views/assemble/processOrder.vue
@@ -4,8 +4,9 @@
             <div>
                 <el-form ref="form" class="form" :inline="true">
                     <el-form-item label="工单号" class="mb-2">
-                        <el-input placeholder="请输入" clearable style="width: 350px" v-model="getForm.SearchModel.wo"
-                            class="input-with-select">
+                        <el-input placeholder="请输入" clearable style="width: 350px"
+                            v-model="getForm.SearchModel.WorkOrder" class="input-with-select"
+                            @keyup.enter.native="handleSearch">
                         </el-input>
                     </el-form-item>
 
@@ -18,12 +19,12 @@
                     </el-form-item>
                     <el-form-item class="mb-2">
                         <el-button type="primary" @click="handleSearch()">查询</el-button>
-                        <el-button type="warning" @click="addVisible=true">生成</el-button>
+                        <el-button type="warning" @click="addVisible = true">生成</el-button>
                     </el-form-item>
                 </el-form>
             </div>
-            <el-table :data="tableData
-                " border :height="tableHeight" style="width: 100%" size="mini">
+            <el-table :data="tableData" border :height="tableHeight" style="width: 100%" size="mini"
+                @row-click="rowDblclick" highlight-current-row>
                 <el-table-column label="序号" width="55" align="center">
                     <template slot-scope="scope">
                         <span>{{
@@ -31,16 +32,18 @@
                         }}</span>
                     </template>
                 </el-table-column>
-
+                <af-table-column prop="GenerateNumber" label="生成批号">
+                </af-table-column>
+                <af-table-column prop="GenerateQty" label="数量"> </af-table-column>
                 <af-table-column prop="WorkOrder" label="工单号"> </af-table-column>
-                <af-table-column prop="WorkOrder" label="数量"> </af-table-column>
-                <af-table-column prop="WorkOrder" label="生产时间"> </af-table-column>
-                <af-table-column prop="OperateUser" label="操作人"> </af-table-column>
-                <af-table-column prop="OperateTime" label="时间"> </af-table-column>
+
+                <af-table-column prop="UpdatedOn" label="生成时间"> </af-table-column>
+                <af-table-column prop="UpdatedBy" label="操作人"> </af-table-column>
+
                 <el-table-column fixed="right" label="操作" width="140" align="center">
                     <template v-slot="{ row }">
-                        <el-button type="primary" size="mini" icon="el-icon-document" @click="handleEdit(row)" />
-                        <el-button type="danger" size="mini" icon="el-icon-delete" @click="handleDelete(row)" />
+                        <el-button type="success" size="mini" @click.stop="handleEdit(row)">导出过程码</el-button>
+                        <!-- <el-button type="danger" size="mini" icon="el-icon-delete" @click="handleDelete(row)" /> -->
                     </template>
                 </el-table-column>
             </el-table>
@@ -53,20 +56,27 @@
             </div>
             <el-tabs v-model="activeName" style="width: 100%">
                 <el-tab-pane label="过程码清单" name="first">
-                    <el-table :data="tableFirst" :height="tableHeight1" border stripe size="mini">
-                        <el-table-column label="序号" width="60" type="index"> </el-table-column>
-                        <el-table-column prop="Code" label="过程码"> </el-table-column>
-                    </el-table></el-tab-pane>
-                </el-tabs>
+                    <el-table :data="tableFirst" ref="myTable" :height="tableHeight1" border stripe size="mini">
+                        <el-table-column label="序号" width="60" type="index" align="center">
+                        </el-table-column>
+                        <el-table-column prop="SN" label="过程码">
+                        </el-table-column> </el-table></el-tab-pane>
+            </el-tabs>
         </el-card>
         <el-dialog :title="'生成'" :visible.sync="addVisible" width="400px" @close="addCancel()">
-
-            <el-form ref="addFormRef" :model="addForm" label-width="auto">
-                <el-form-item label="工单号">
-                    <el-input v-model="addForm.wo" placeholder="请输入工单号" width="100%" />
+            <el-form ref="addFormRef" :model="addForm" label-width="auto" :rules="rules">
+                <el-form-item label="物料编码" prop="MaterialName">
+                    <el-select v-model="addForm.MaterialName" filterable style="width: 100%">
+                        <el-option v-for="item in MaterialList" :key="item.value" :label="item.label"
+                            :value="item.value">
+                        </el-option>
+                    </el-select>
                 </el-form-item>
-                <el-form-item label="数量">
-                    <el-input v-model="addForm.num" placeholder="请输入数量" width="100%"/>
+                <el-form-item label="工单号" prop="WorkOrder">
+                    <el-input v-model="addForm.WorkOrder" placeholder="请输入工单号" width="100%" />
+                </el-form-item>
+                <el-form-item label="数量" prop="GenerateQty">
+                    <el-input v-model="addForm.GenerateQty" placeholder="请输入数量" width="100%" />
                 </el-form-item>
             </el-form>
             <span slot="footer" class="dialog-footer">
@@ -78,6 +88,11 @@
 </template>
 
 <script>
+import {
+    ExecuteAssGenerateSN,
+    GetAssGenerateRecord,
+    GetAssGenerateSN,
+} from "@/api/assemble";
 import { getToken } from "@/utils/auth";
 import {
     shortcuts,
@@ -85,22 +100,24 @@ import {
     setTodayDate,
     setLastDate,
 } from "@/utils/dataMenu";
+import { exportTableToExcel } from "@/utils/exportExcel";
+import dayjs from "dayjs";
 export default {
     data() {
         return {
             getForm: {
-
                 PageIndex: 1,
                 PageSize: 50,
                 SearchModel: {
-                    wo: "",
-                    OperateStartTime: "",
-                    OperateEndTime: "",
+                    WorkOrder: "",
                 },
+                StartTime: "",
+                EndTime: "",
             },
             dateInterval: [],
             pickerOptions: {
                 shortcuts: shortcuts,
+                disabledDate: disabledDate,
             },
             tableHeight: 0,
             tableHeight1: 0,
@@ -109,28 +126,43 @@ export default {
             tableFirst: [],
             activeName: "first",
             addForm: {
-                wo: "",
-                user: getToken(),
-                num: "",
+                WorkOrder: "",
+                CreatedBy: getToken(),
+                GenerateQty: "",
+                MaterialName: "1022059902400",
             },
             addVisible: false,
+            MaterialList: [
+                {
+                    value: "1022059902400",
+                    label: "1022059902400",
+                },
+            ],
+            rules: {
+                MaterialName: [
+                    { required: true, message: "物料编码不能为空", change: "blur" },
+                ],
+                GenerateQty: [
+                    { required: true, message: "数量不能为空", change: "blur" },
+                ],
+            },
+            generateNumber: "",
         };
     },
     watch: {
         dateInterval(newdata) {
             if (newdata != null) {
-                this.getForm.SearchModel.OperateStartTime = newdata[0];
-                this.getForm.SearchModel.OperateEndTime = newdata[1];
+                this.getForm.StartTime = newdata[0];
+                this.getForm.EndTime = newdata[1];
             } else {
-                this.getForm.SearchModel.OperateStartTime = "";
-                this.getForm.SearchModel.OperateEndTime = "";
+                this.getForm.StartTime = "";
+                this.getForm.EndTime = "";
             }
         },
-
     },
     beforeMount() {
         this.getScreenHeight();
-
+        this.getData();
     },
 
     mounted() {
@@ -143,13 +175,94 @@ export default {
         window.removeEventListener("resize", this.getScreenHeight);
     },
     methods: {
+        getData() {
+            GetAssGenerateRecord(this.getForm).then((res) => {
+                if (res.Data.list.length === 0) {
+                } else {
+                    this.tableData = res.Data.list;
+                    this.total = res.Data.Total;
+                }
 
+                this.tableFirst = [];
+            });
+        },
+        handleSearch() {
+            this.getForm.PageIndex = 1;
+            this.getData();
+        },
         addCancel() {
             this.addVisible = false;
             this.addForm.wo = "";
             this.addForm.num = "";
         },
-        handleSubmit(){
+        handleSubmit() {
+            this.$refs.addFormRef.validate((valid) => {
+                if (valid) {
+                    ExecuteAssGenerateSN(this.addForm).then((res) => {
+                        if (res.Success) {
+                            this.$notify({
+                                title: "提示信息",
+                                message: res.Msg,
+                                type: "success",
+                            });
+                            this.addVisible = false;
+                            this.addForm.WorkOrder = "";
+                            this.addForm.GenerateQty = "";
+                            this.getData();
+                        } else {
+                            this.$notify({
+                                title: "提示信息",
+                                message: res.Msg,
+                                type: "error",
+                            });
+                        }
+                    });
+                }
+            });
+        },
+        rowDblclick(row) {
+            this.activeName = "first";
+            GetAssGenerateSN({ GenerateNumber: row.GenerateNumber }).then((res) => {
+                this.tableFirst = res.Data;
+            });
+        },
+        handleEdit(row) {
+            this.generateNumber = row.GenerateNumber;
+            GetAssGenerateSN({ GenerateNumber: row.GenerateNumber }).then((res) => {
+                if (res.Data.length === 0) {
+                    this.$notify({
+                        title: "提示信息",
+                        message: "没有过程码数据",
+                        type: "warning",
+                    });
+                    return;
+                } else {
+
+                    exportTableToExcel({
+                        tableRef: this.$refs.myTable,
+                        fetchAllData: this.fetchAllUsers,
+                        fileName: `${this.generateNumber}_${dayjs().format("YYYYMMDDHHmmss")}`,
+                        styles: {
+                            headerBgColor: "", // 灰色表头
+                            headerFont: {
+                                color: { argb: "" }, // 红色文字
+                                bold: true,
+                                size: 14,
+                            }, // 白色文字
+                            cell: { numFmt: "@" }, // 强制文本格式
+                        },
+                    });
+                }
+            });
+        },
+        async fetchAllUsers() {
+
+            let data = await GetAssGenerateSN({ GenerateNumber: this.generateNumber }).then((res) => {
+
+                return res.Data
+
+            });
+            return data;
 
         },
         handleSizeChange(value) {
@@ -164,10 +277,11 @@ export default {
         getScreenHeight() {
             this.$nextTick(() => {
                 this.tableHeight = (window.innerHeight - 148 - 68 - 68 + 80 - 70) * 0.6;
-                this.tableHeight1 = (window.innerHeight - 148 - 68 + 80 - 50 - 70) * 0.4;
-            })
-        }
-    }
+                this.tableHeight1 =
+                    (window.innerHeight - 148 - 68 + 80 - 50 - 70) * 0.4;
+            });
+        },
+    },
 };
 </script>
 
